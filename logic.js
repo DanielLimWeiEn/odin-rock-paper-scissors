c40980375ecaa7f0569223dea995d455c84a3667
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-05-06 19:47:29 +0800

    Create computerPlay function that simulates a computer move

--- logic.js
+++ logic.js
@@ -1,3 +1,21 @@
+/**
+ * Gets a random integer between min and max values.
+ */
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+/**
+ * Returns 'Rock', 'Paper' or 'Scissors' randomly,
+ * representing the computer making a move.
+ */
 function computerPlay() {
-    
+    let val = getRandomInteger(0, 3);
+    if (val === 0) {
+        return "Rock";
+    } else if (val === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
 }
